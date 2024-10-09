98b857f632c5fe231fbf131a4dadc4de1ba95bf2
nguyenanh <nguyenwnhstranquang@gmail.com>
2024-10-09 12:32:13 +0700

    update new PC file code

--- mile2/00_src/pc.sv
+++ mile2/00_src/pc.sv
@@ -1,9 +1,12 @@
-module pc(
-    input logic clk_i,  
-    input logic [31:0] next_pc_i, 
-    output logic [31:0] pc_o
-);    
-    always_ff @( posedge clk_i ) begin
-        pc_o <= next_pc_i; 
+module PC (
+    input logic i_clk, 
+    input logic [31:0] i_pc, 
+    output logic [31:0] o_pc
+);
+    
+    /*Update new output at each positive edge clock*/
+    always_ff @( posedge i_clk ) begin 
+        o_pc <= i_pc; 
     end
-endmodule 
+    
+endmodule
