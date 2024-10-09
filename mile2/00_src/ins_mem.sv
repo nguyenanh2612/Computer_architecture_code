9516057e263867e28e882d515cef3c937ffca071
nguyenanh <nguyenwnhstranquang@gmail.com>
2024-10-09 13:13:35 +0700

    update file instruction mem or rom

---
+++ mile2/00_src/ins_mem.sv
@@ -0,0 +1,14 @@
+module ins_mem ( 
+    input logic [31:0] i_address, 
+    output logic [31:0] o_data
+);
+
+    /*Registers store the data */
+    logic [31:0] ins [0:2047]; 
+    
+    initial begin
+        $readmemh("instruction.txt",ins); 
+    end
+	 
+	 assign o_data = ins[i_address]; 
+endmodule 
