e046fad1f26b3b8433e548ddd7974cb0b9cc5fab
nguyenanh <nguyenwnhstranquang@gmail.com>
2024-10-11 17:52:37 +0700

    update new alu with new algorithm shift

--- mile2/00_src/alu.sv
+++ mile2/00_src/alu.sv
@@ -45,117 +45,117 @@ module alu (
         end
         //SLL
         4'd7: begin
-            case (i_operand_b[4:0])
-            5'd0 : o_alu_data = i_operand_a; 
-            5'd1 : o_alu_data = {i_operand_a[30:0],1'd0};
-            5'd2 : o_alu_data = {i_operand_a[29:0],2'd0};
-            5'd3 : o_alu_data = {i_operand_a[28:0],3'd0};
-            5'd4 : o_alu_data = {i_operand_a[27:0],4'd0};
-            5'd5 : o_alu_data = {i_operand_a[26:0],5'd0};
-            5'd6 : o_alu_data = {i_operand_a[25:0],6'd0};
-            5'd7 : o_alu_data = {i_operand_a[24:0],7'd0};
-            5'd8 : o_alu_data = {i_operand_a[23:0],8'd0};
-            5'd9 : o_alu_data = {i_operand_a[22:0],9'd0};
-            5'd10: o_alu_data = {i_operand_a[21:0],10'd0};
-            5'd11: o_alu_data = {i_operand_a[20:0],11'd0};
-            5'd12: o_alu_data = {i_operand_a[19:0],12'd0};
-            5'd13: o_alu_data = {i_operand_a[18:0],13'd0};
-            5'd14: o_alu_data = {i_operand_a[17:0],14'd0};
-            5'd15: o_alu_data = {i_operand_a[16:0],15'd0};
-            5'd16: o_alu_data = {i_operand_a[15:0],16'd0};
-            5'd17: o_alu_data = {i_operand_a[14:0],17'd0};
-            5'd18: o_alu_data = {i_operand_a[13:0],18'd0};
-            5'd19: o_alu_data = {i_operand_a[12:0],19'd0};
-            5'd20: o_alu_data = {i_operand_a[11:0],20'd0};
-            5'd21: o_alu_data = {i_operand_a[10:0],21'd0};
-            5'd22: o_alu_data = {i_operand_a[9:0],22'd0};
-            5'd23: o_alu_data = {i_operand_a[8:0],23'd0};
-            5'd24: o_alu_data = {i_operand_a[7:0],24'd0};
-            5'd25: o_alu_data = {i_operand_a[6:0],25'd0};
-            5'd26: o_alu_data = {i_operand_a[5:0],26'd0};
-            5'd27: o_alu_data = {i_operand_a[4:0],27'd0};
-            5'd28: o_alu_data = {i_operand_a[3:0],28'd0};
-            5'd29: o_alu_data = {i_operand_a[2:0],29'd0};
-            5'd30: o_alu_data = {i_operand_a[1:0],30'd0};
-            5'd31: o_alu_data = {i_operand_a[0]};
-                default: o_alu_data = 32'd0; 
-            endcase 
+            logic [31:0] temp_sll;
+
+            temp_sll = i_operand_a;
+
+            if (i_operand_b[0]) begin
+                temp_sll = {temp_sll[30:0],1'd0};
+            end else begin
+                temp_sll = temp_sll; 
+            end
+
+            if (i_operand_b[1]) begin
+                temp_sll = {temp_sll[29:0],2'd0};
+            end else begin
+                temp_sll = temp_sll; 
+            end
+
+            if (i_operand_b[2]) begin
+                temp_sll = {temp_sll[27:0],4'd0};
+            end else begin
+                temp_sll = temp_sll; 
+            end
+
+            if (i_operand_b[3]) begin
+                temp_sll = {temp_sll[23:0],8'd0};
+            end else begin
+                temp_sll = temp_sll; 
+            end
+
+            if (i_operand_b[4]) begin
+                temp_sll = {temp_sll[15:0],16'd0};
+            end else begin
+                temp_sll = temp_sll; 
+            end
+
+            o_alu_data = temp_sll; 
         end
-        //STL
+        //SRL
         4'd8: begin
-            case (i_operand_b[4:0])
-            5'd0 : o_alu_data = i_operand_a; 
-            5'd1 : o_alu_data = {1'd0,i_operand_a[31:1]};
-            5'd2 : o_alu_data = {2'd0,i_operand_a[31:2]};
-            5'd3 : o_alu_data = {3'd0,i_operand_a[31:3]};
-            5'd4 : o_alu_data = {4'd0,i_operand_a[31:4]};
-            5'd5 : o_alu_data = {5'd0,i_operand_a[31:5]};
-            5'd6 : o_alu_data = {6'd0,i_operand_a[31:6]};
-            5'd7 : o_alu_data = {7'd0,i_operand_a[31:7]};
-            5'd8 : o_alu_data = {8'd0,i_operand_a[31:8]};
-            5'd9 : o_alu_data = {9'd0,i_operand_a[31:9]};
-            5'd10: o_alu_data = {10'd0,i_operand_a[31:10]};
-            5'd11: o_alu_data = {11'd0,i_operand_a[31:11]};
-            5'd12: o_alu_data = {12'd0,i_operand_a[31:12]};
-            5'd13: o_alu_data = {13'd0,i_operand_a[31:13]};
-            5'd14: o_alu_data = {14'd0,i_operand_a[31:14]};
-            5'd15: o_alu_data = {15'd0,i_operand_a[31:15]};
-            5'd16: o_alu_data = {16'd0,i_operand_a[31:16]};
-            5'd17: o_alu_data = {17'd0,i_operand_a[31:17]};
-            5'd18: o_alu_data = {18'd0,i_operand_a[31:18]};
-            5'd19: o_alu_data = {19'd0,i_operand_a[31:19]};
-            5'd20: o_alu_data = {20'd0,i_operand_a[31:20]};
-            5'd21: o_alu_data = {21'd0,i_operand_a[31:21]};
-            5'd22: o_alu_data = {22'd0,i_operand_a[31:22]};
-            5'd23: o_alu_data = {23'd0,i_operand_a[31:23]};
-            5'd24: o_alu_data = {24'd0,i_operand_a[31:24]};
-            5'd25: o_alu_data = {25'd0,i_operand_a[31:25]};
-            5'd26: o_alu_data = {26'd0,i_operand_a[31:26]};
-            5'd27: o_alu_data = {27'd0,i_operand_a[31:27]};
-            5'd28: o_alu_data = {28'd0,i_operand_a[31:28]};
-            5'd29: o_alu_data = {29'd0,i_operand_a[31:29]};
-            5'd30: o_alu_data = {30'd0,i_operand_a[31:30]};
-            5'd31: o_alu_data = {31'd0,i_operand_a[31]};
-                default: o_alu_data = 0;
-            endcase 
+            logic [31:0] temp_srl;
+
+            temp_srl = i_operand_a;
+
+            if (i_operand_b[0]) begin
+                temp_srl = {1'd0,temp_srl[31:1]};
+            end else begin
+                temp_srl = temp_srl; 
+            end
+
+            if (i_operand_b[1]) begin
+                temp_srl = {2'd0,temp_srl[31:2]};
+            end else begin
+                temp_srl = temp_srl; 
+            end
+
+            if (i_operand_b[2]) begin
+                temp_srl = {4'd0,temp_srl[31:4]};
+            end else begin
+                temp_srl = temp_srl; 
+            end
+
+            if (i_operand_b[3]) begin
+                temp_srl = {8'd0,temp_srl[31:8]};
+            end else begin
+                temp_srl = temp_srl; 
+            end
+
+            if (i_operand_b[4]) begin
+                temp_srl = {16'd0,temp_srl[31:16]};
+            end else begin
+                temp_srl = temp_srl; 
+            end
+
+            o_alu_data = temp_srl;
         end
         //SRA
         4'd9: begin
-            case (i_operand_b[4:0])
-            5'd0 : o_alu_data = i_operand_a; 
-            5'd1 : o_alu_data = {arimethic_signed_shift[0],i_operand_a[31:1]};
-            5'd2 : o_alu_data = {arimethic_signed_shift[1:0],i_operand_a[31:2]};
-            5'd3 : o_alu_data = {arimethic_signed_shift[2:0],i_operand_a[31:3]};
-            5'd4 : o_alu_data = {arimethic_signed_shift[3:0],i_operand_a[31:4]};
-            5'd5 : o_alu_data = {arimethic_signed_shift[4:0],i_operand_a[31:5]};
-            5'd6 : o_alu_data = {arimethic_signed_shift[5:0],i_operand_a[31:6]};
-            5'd7 : o_alu_data = {arimethic_signed_shift[6:0],i_operand_a[31:7]};
-            5'd8 : o_alu_data = {arimethic_signed_shift[7:0],i_operand_a[31:8]};
-            5'd9 : o_alu_data = {arimethic_signed_shift[8:0],i_operand_a[31:9]};
-            5'd10: o_alu_data = {arimethic_signed_shift[9:0],i_operand_a[31:10]};
-            5'd11: o_alu_data = {arimethic_signed_shift[10:0],i_operand_a[31:11]};
-            5'd12: o_alu_data = {arimethic_signed_shift[11:0],i_operand_a[31:12]};
-            5'd13: o_alu_data = {arimethic_signed_shift[12:0],i_operand_a[31:13]};
-            5'd14: o_alu_data = {arimethic_signed_shift[13:0],i_operand_a[31:14]};
-            5'd15: o_alu_data = {arimethic_signed_shift[14:0],i_operand_a[31:15]};
-            5'd16: o_alu_data = {arimethic_signed_shift[15:0],i_operand_a[31:16]};
-            5'd17: o_alu_data = {arimethic_signed_shift[16:0],i_operand_a[31:17]};
-            5'd18: o_alu_data = {arimethic_signed_shift[17:0],i_operand_a[31:18]};
-            5'd19: o_alu_data = {arimethic_signed_shift[18:0],i_operand_a[31:19]};
-            5'd20: o_alu_data = {arimethic_signed_shift[19:0],i_operand_a[31:20]};
-            5'd21: o_alu_data = {arimethic_signed_shift[20:0],i_operand_a[31:21]};
-            5'd22: o_alu_data = {arimethic_signed_shift[21:0],i_operand_a[31:22]};
-            5'd23: o_alu_data = {arimethic_signed_shift[22:0],i_operand_a[31:23]};
-            5'd24: o_alu_data = {arimethic_signed_shift[23:0],i_operand_a[31:24]};
-            5'd25: o_alu_data = {arimethic_signed_shift[24:0],i_operand_a[31:25]};
-            5'd26: o_alu_data = {arimethic_signed_shift[25:0],i_operand_a[31:26]};
-            5'd27: o_alu_data = {arimethic_signed_shift[26:0],i_operand_a[31:27]};
-            5'd28: o_alu_data = {arimethic_signed_shift[27:0],i_operand_a[31:28]};
-            5'd29: o_alu_data = {arimethic_signed_shift[28:0],i_operand_a[31:29]};
-            5'd30: o_alu_data = {arimethic_signed_shift[29:0],i_operand_a[31:30]};
-            5'd31: o_alu_data = {arimethic_signed_shift[30:0],i_operand_a[31]};
-                default: o_alu_data = 0; 
-            endcase
+            logic [31:0] temp_sra;
+
+            temp_sra = i_operand_a;
+
+            if (i_operand_b[0]) begin
+                temp_sra = {arimethic_signed_shift[0],temp_sra[31:1]};
+            end else begin
+                temp_sra = temp_sra; 
+            end
+
+            if (i_operand_b[1]) begin
+                temp_sra = {arimethic_signed_shift[1:0],temp_sra[31:2]};
+            end else begin
+                temp_sra = temp_sra; 
+            end
+
+            if (i_operand_b[2]) begin
+                temp_sra = {arimethic_signed_shift[3:0],temp_sra[31:4]};
+            end else begin
+                temp_sra = temp_sra; 
+            end
+
+            if (i_operand_b[3]) begin
+                temp_sra = {arimethic_signed_shift[7:0],temp_sra[31:8]};
+            end else begin
+                temp_sra = temp_sra; 
+            end
+
+            if (i_operand_b[4]) begin
+                temp_sra = {arimethic_signed_shift[15:0],temp_sra[31:16]};
+            end else begin
+                temp_sra = temp_sra; 
+            end
+
+            o_alu_data = temp_sra;
         end
             default: o_alu_data = 32'd0; 
         endcase
